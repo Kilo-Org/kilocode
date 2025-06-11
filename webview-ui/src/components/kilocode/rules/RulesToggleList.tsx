657d3a2e24d9c3c6821238fe5ea9d0959bb42bb0
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2025-06-11 13:52:11 +0200

    Make strings translatable

--- webview-ui/src/components/kilocode/rules/RulesToggleList.tsx
+++ webview-ui/src/components/kilocode/rules/RulesToggleList.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next"
 import NewRuleRow from "./NewRuleRow"
 import RuleRow from "./RuleRow"
 
@@ -12,6 +13,7 @@ const RulesToggleList = ({
 	isGlobal: boolean
 	ruleType: "rule" | "workflow"
 }) => {
+	const { t } = useTranslation()
 	return (
 		<div className="flex flex-col gap-0">
 			{rules.length > 0 ? (
@@ -20,7 +22,9 @@ const RulesToggleList = ({
 				))
 			) : (
 				<div className="flex flex-col items-center gap-3 my-3 text-[var(--vscode-descriptionForeground)]">
-					{ruleType === "workflow" ? "No workflows found" : "No rules found"}
+					{ruleType === "workflow"
+						? t("kilocode:rules.emptyState.noWorkflowsFound")
+						: t("kilocode:rules.emptyState.noRulesFound")}
 				</div>
 			)}
 			<NewRuleRow isGlobal={isGlobal} ruleType={ruleType} />
