18a0755c5ee0bd4a0d3592462b2928af54f4f37e
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2025-06-11 14:46:45 +0200

    Remove some unnecessary cruft

--- webview-ui/src/components/kilocode/rules/RulesToggleList.tsx
+++ webview-ui/src/components/kilocode/rules/RulesToggleList.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from "react-i18next"
 import NewRuleRow from "./NewRuleRow"
 import RuleRow from "./RuleRow"
 
@@ -12,24 +11,14 @@ const RulesToggleList = ({
 	toggleRule: (rulePath: string, enabled: boolean) => void
 	isGlobal: boolean
 	ruleType: "rule" | "workflow"
-}) => {
-	const { t } = useTranslation()
-	return (
-		<div className="flex flex-col gap-0">
-			{rules.length > 0 ? (
-				rules.map(([rulePath, enabled]) => (
-					<RuleRow key={rulePath} rulePath={rulePath} enabled={enabled} toggleRule={toggleRule} />
-				))
-			) : (
-				<div className="flex flex-col items-center gap-3 my-3 text-[var(--vscode-descriptionForeground)]">
-					{ruleType === "workflow"
-						? t("kilocode:rules.emptyState.noWorkflowsFound")
-						: t("kilocode:rules.emptyState.noRulesFound")}
-				</div>
-			)}
-			<NewRuleRow isGlobal={isGlobal} ruleType={ruleType} />
-		</div>
-	)
-}
+}) => (
+	<div className="flex flex-col gap-0">
+		{rules.length > 0 &&
+			rules.map(([rulePath, enabled]) => (
+				<RuleRow key={rulePath} rulePath={rulePath} enabled={enabled} toggleRule={toggleRule} />
+			))}
+		<NewRuleRow isGlobal={isGlobal} ruleType={ruleType} />
+	</div>
+)
 
 export default RulesToggleList
