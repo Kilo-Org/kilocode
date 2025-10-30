40ab73e3d87fdde7a95d2901088f4ef1bfc85b0b
oliver-14203 <oburton.au@users.noreply.github.com>
2025-10-30 19:05:54 +0800

    Reset formatting changes to upstream versions

--- src/services/continuedev/core/autocomplete/CompletionProvider.ts
+++ src/services/continuedev/core/autocomplete/CompletionProvider.ts
@@ -55,6 +55,7 @@ export class CompletionProvider {
 			return undefined
 		}
 
+
 		// Temporary fix for JetBrains autocomplete bug as described in https://github.com/continuedev/continue/pull/3022
 		if (llm.model === undefined && llm.completionOptions?.model !== undefined) {
 			llm.model = llm.completionOptions.model
@@ -75,7 +76,7 @@ export class CompletionProvider {
 			llm.useLegacyCompletionsEndpoint = true
 		}
 
-		console.log("using LLM", llm)
+		console.log('using LLM', llm)
 		return llm
 	}
 
@@ -137,7 +138,7 @@ export class CompletionProvider {
 			// Create abort signal if not given
 			if (!token) {
 				const controller = this.loggingService.createAbortController(input.completionId)
-				console.log("creating abort token because none given")
+				console.log('creating abort token because none given')
 				token = controller.signal
 			}
 			const startTime = Date.now()
@@ -213,11 +214,11 @@ export class CompletionProvider {
 
 				// Don't postprocess if aborted
 				if (token.aborted) {
-					console.log("aborted")
+					console.log('aborted')
 					return undefined
 				}
 
-				console.log("raw completion", completion)
+				console.log('raw completion', completion)
 
 				const processedCompletion = helper.options.transform
 					? postprocessCompletion({
