1595c9853659bdcb2e33408e542343ad864db2bd
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2026-01-28 16:36:43 +0100

    PaidModelScreen

--- webview-ui/src/components/kilocode/chat/IdeaSuggestionsBox.tsx
+++ webview-ui/src/components/kilocode/chat/IdeaSuggestionsBox.tsx
@@ -1,14 +1,11 @@
 import { FolderNoHistoryScreen } from "@/components/welcome/screens/FolderNoHistoryScreen"
 import { NoFolderNoHistoryScreen } from "@/components/welcome/screens/NoFolderNoHistoryScreen"
-import { PaidModelScreen } from "@/components/welcome/screens/PaidModelScreen"
 import { vscode } from "@/utils/vscode"
 import { useEffect, useState } from "react"
 
 export const IdeaSuggestionsBox = () => {
 	const [hasOpenFolder, setHasOpenFolder] = useState<boolean>(false)
 	const [hasSessionHistory, setHasSessionHistory] = useState<boolean>(false)
-	const [showPaidModelScreen, _setShowPaidModelScreen] = useState<boolean>(false)
-	const [supportsKiloGateway, _setSupportsKiloGateway] = useState<boolean>(false)
 
 	useEffect(() => {
 		vscode.postMessage({ type: "checkWorkspaceState" })
@@ -36,10 +33,6 @@ export const IdeaSuggestionsBox = () => {
 
 	if (hasOpenFolder && !hasSessionHistory) {
 		return <FolderNoHistoryScreen />
-	}
-
-	if (showPaidModelScreen) {
-		return <PaidModelScreen supportsKiloGateway={supportsKiloGateway} />
 	}
 
 	return null
